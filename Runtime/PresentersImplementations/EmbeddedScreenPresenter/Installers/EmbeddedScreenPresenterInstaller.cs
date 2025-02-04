eb5c5b7b1c8910605b612aea7ce6d5b595ed29a1
FedeJIS <fede97cem@gmail.com>
2025-02-04 13:22:50 -0300

    [A] Added embedded presenters

---
+++ Runtime/PresentersImplementations/EmbeddedScreenPresenter/Installers/EmbeddedScreenPresenterInstaller.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInjector.Core;
+using DependencyInjector.Installers;
+
+namespace ScreenNavigators.Core
+{
+    public abstract class EmbeddedScreenPresenterInstaller<TScreenPresenter> : SingleMonoInstaller<IScreenPresenter>
+    {
+        [Inject] private TScreenPresenter _screenPresenter;
+
+        protected override IScreenPresenter GetData()
+        {
+            return GetEmbeddedScreenPresenter(_screenPresenter);
+        }
+
+        protected abstract EmbeddedScreenPresenter<TScreenPresenter> GetEmbeddedScreenPresenter(
+            TScreenPresenter screenPresenter);
+    }
+}
