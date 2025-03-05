b37038afa10e980f4fd159797f4e26b86d150c5b
FedeJIS <fede97cem@gmail.com>
2025-03-05 12:10:07 -0300

    [A] Added controller to auto close windows on demand

---
+++ Runtime/ScreenActionNotifier/Core/Installers/ActiveCloseScreenControllerInstaller.cs
@@ -0,0 +1,20 @@
+﻿using DependencyInjector.Core;
+using DependencyInjector.Installers;
+using MVVM.Core;
+using ScreenActionNotifier.Core;
+using UnityEngine;
+
+namespace ScreenNavigators.Core
+{
+    public class ActiveCloseScreenControllerInstaller : SingleMonoInstaller<ActiveCloseScreenController>
+    {
+        [SerializeField] private EventViewModelSO _openRequestEventViewModelSO;
+        [SerializeField] private bool _isScreenActive;
+        
+        [Inject] private IScreenPresenter _screenPresenter;
+        protected override ActiveCloseScreenController GetData()
+        {
+            return new ActiveCloseScreenController(_openRequestEventViewModelSO.GetEventViewModel(), _screenPresenter, _isScreenActive);
+        }
+    }
+}
---
+++ Runtime/ScreenActionNotifier/Core/InterfaceAdapters/Controllers/ActiveCloseScreenController.cs
@@ -0,0 +1,31 @@
+﻿using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using ScreenNavigators.Core;
+
+namespace ScreenActionNotifier.Core
+{
+    public class ActiveCloseScreenController : Controller
+    {
+        private IScreenPresenter _screenPresenter;
+        private bool _isScreenActive;
+        
+        public ActiveCloseScreenController(IEventViewModel eventViewModel, IScreenPresenter screenPresenter, bool isScreenActive) : base(eventViewModel)
+        {
+            _screenPresenter = screenPresenter;
+            _isScreenActive = isScreenActive;
+        }
+
+        public override void Execute()
+        {
+            if (_isScreenActive)
+            {
+                _screenPresenter.Close();
+                _isScreenActive = false;
+                return;
+            }
+            
+            _isScreenActive = true;
+            _screenPresenter.Present();
+        }
+    }
+}
