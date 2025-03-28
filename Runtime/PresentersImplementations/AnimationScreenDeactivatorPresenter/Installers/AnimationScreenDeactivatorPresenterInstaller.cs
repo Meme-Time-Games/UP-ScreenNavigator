a0d34cca2d89fc0cc5df812a572540c7217611c7
Lalo <laurencioberro@hotmail.com>
2025-03-28 12:13:43 -0300

    [A] Added AnimationScreenDeactivatorPresenter

---
+++ Runtime/PresentersImplementations/AnimationScreenDeactivatorPresenter/Installers/AnimationScreenDeactivatorPresenterInstaller.cs
@@ -0,0 +1,23 @@
+﻿using DependencyInjector.Installers;
+using UnityEngine;
+
+namespace ScreenNavigators.Core
+{
+    public class AnimationScreenDeactivatorPresenterInstaller : SingleMonoInstaller<IScreenDeactivatorPresenter>
+    {
+        [Header("References")]
+        [SerializeField] private GameObject _gameObjectToClose;
+
+        [Header("Config")]
+        [SerializeField] private AnimationClip _animationClip;
+        
+        protected override IScreenDeactivatorPresenter GetData()
+        {
+            GameObject animationScreenDeactivatorPresenterGameObject = new GameObject("AnimationScreenDeactivatorPresenter");
+            AnimationScreenDeactivatorPresenter animationScreenDeactivatorPresenter = animationScreenDeactivatorPresenterGameObject.AddComponent<AnimationScreenDeactivatorPresenter>();
+            animationScreenDeactivatorPresenter.Install(_gameObjectToClose, _animationClip.length);
+            
+            return animationScreenDeactivatorPresenter;
+        }
+    }
+}
---
+++ Runtime/PresentersImplementations/AnimationScreenDeactivatorPresenter/InterfaceAdapters/Presenters/AnimationScreenDeactivatorPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ScreenNavigators.Core
+{
+    public class AnimationScreenDeactivatorPresenter : MonoBehaviour, IScreenDeactivatorPresenter
+    {
+        private GameObject _gameObjectToClose;
+        private WaitForSeconds _waitForSeconds;
+        
+        public void Install(GameObject gameObjectToClose, float animationTime)
+        {
+            _gameObjectToClose = gameObjectToClose;
+            _waitForSeconds = new WaitForSeconds(animationTime);
+        }
+        
+        public void Close()
+        {
+            StartCoroutine(CloseAfterTime());
+        }
+
+        public IEnumerator CloseAfterTime()
+        {
+            yield return _waitForSeconds;
+            _gameObjectToClose.SetActive(false);
+        }
+    }
+}
